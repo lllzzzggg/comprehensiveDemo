1046c91381250c9233a74230508487e54adf7ec4
a1178013314 <1178013314@qq.com>
2018-03-24 19:52:08 +0800

    切图练习

---
+++ qieTu/js/main.js
@@ -0,0 +1,15 @@
+(function(doc,win){
+    var docEl = doc.documentElement,  //文档根标签
+        resizeEvent =  'orientationchange' in window ? 'orientationchang' :'resize'; //viewport变化事件源
+    var rescale = function(){
+        //重置方法
+        win.clientWidth = docEl.clientWidth;
+        if (!win.clientWidth) return;
+        // 改变DOM根节点fontSize大小的值;
+        // (屏幕宽度/设计图宽度) = 缩放或扩大的比例值;
+        docEl.style.fontSize = 40 * (win.clientWidth / 640) + 'px';
+    }
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvent, rescale, false);
+    doc.addEventListener('DOMContentLoaded', rescale, false);
+})(document, window);
